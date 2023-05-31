61ced56d81e8d5c9f2587afd7ab39b454989131a
Darlan <darlansilv4@gmail.com>
2023-05-30 22:46:11 -0300

    Criando primeiras rotas: Home, About

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/global.css'
+
+// Componentes
 import { Home } from './components/Home'
+import { About } from './components/About'
+
+// Rotas
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
